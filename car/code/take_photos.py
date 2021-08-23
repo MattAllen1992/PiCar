fb2e5e7b3d6eeb3f675afe5b7afff6ca49694d75
MattAllen1992 <matthewaallen92@gmail.com>
2021-08-23 18:49:45 +0100

    fisheye calibration images

--- car/code/take_photos.py
+++ car/code/take_photos.py
@@ -1,5 +1,5 @@
 import cv2
-import datetime
+from datetime import datetime
 
 # assign camera
 cap = cv2.VideoCapture(0)
@@ -7,12 +7,12 @@ cap = cv2.VideoCapture(0)
 # show RGB image and capture when user presses 'q'
 while(True):
     ret, frame = cap.read()
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2GBRA)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
     cv2.imshow('RGB Image', rgb)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         now = datetime.now()
         now = now.strftime('%m%d%Y_%H%M%S')
-        out = cv2.imwrite('img_' + now + '.jpg')
+        out = cv2.imwrite('/home/pi/work/PiCar/car/data/images/checkerboard_' + now + '.jpg', frame)
         break
 
 # close and release all
